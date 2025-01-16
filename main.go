89f5ca53d9083ce7524e30c7b106d2e86f7d3a1f
root <1149387619@qq.com>
2025-01-16 19:16:12 +0800

    Implement chunks for on-disk server

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"chukcha/server"
 	"chukcha/web"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 var (
@@ -16,22 +19,22 @@ var (
 func main() {
 	flag.Parse()
 	var backend web.Storage
-	// if *inmem {
-	backend = &server.InMemory{}
-	// } else {
-	// 	if *dirname == "" {
-	// 		log.Fatalf("The flag `--dirname` must be provided")
-	// 	}
-	// 	filename := filepath.Join(*dirname, "write_test")
-	// 	fmt.Println(filename)
-	// 	fp, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0666)
-	// 	if err != nil {
-	// 		log.Fatalf("Could noe create test file %q: %s", filename, err)
-	// 	}
-	// 	defer fp.Close()
-	// 	os.Remove(fp.Name())
-	// 	backend = server.NewOnDisk(*dirname)
-	// }
+	if *inmem {
+		backend = &server.InMemory{}
+	} else {
+		if *dirname == "" {
+			log.Fatalf("The flag `--dirname` must be provided")
+		}
+		filename := filepath.Join(*dirname, "write_test")
+		fmt.Println(filename)
+		fp, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0666)
+		if err != nil {
+			log.Fatalf("Could noe create test file %q: %s", filename, err)
+		}
+		defer fp.Close()
+		os.Remove(fp.Name())
+		backend = server.NewOnDisk(*dirname)
+	}
 	s := web.NewServer(backend, *port)
 	log.Printf("Listening connections")
 	s.Serve()
